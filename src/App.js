664d9949f3b4710537e1187523de3e3299957040
alpeshlata <alpeshlata1818@gmail.com>
2024-06-01 15:14:59 +0800

    create new project which is NewsDragon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component } from 'react'
+import NavBar from './/component/NavBar'
+import NewsItems from './NewsItems';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <NavBar/>
+          <div className="container my-4">
+            <h1>Top Higlith Dragon News</h1>
+              <div className="row my-4">
+                <div className="col md-3">
+                    <NewsItems title="Title" description="Description"/>
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="Title" description="Description"/>
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="Title" description="Description"/>
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="Title" description="Description"/>
+                </div>
+              </div>
+          
+          </div>
+      </div>
+    )
+  }
 }
-
-export default App;

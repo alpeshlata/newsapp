1637ced01e30e59be867c5fd604da9bc9329cee7
alpeshlata <alpeshlata1818@gmail.com>
2024-06-04 10:08:24 +0800

    Fix package.json error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import React, { Component } from 'react'
-import NavBar from './/component/NavBar'
+import React, { Component } from 'react';
+import NavBar from './component/NavBar';
 import News from './/component/News';
 
 export default class App extends Component {
